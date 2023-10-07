f49768355c0756b690bfdb5dd291de5b47dcb506
Onismond <67899826+Onismond@users.noreply.github.com>
2023-10-07 20:12:39 +0000

    Update

--- SMS/App.xaml.cs
+++ SMS/App.xaml.cs
@@ -16,6 +16,7 @@ using SMS.Util.Network;
 using SMS.Util.Stores;
 using Microsoft.EntityFrameworkCore;
 using SMS.Views;
+using LiveChartsCore;
 
 namespace SMS
 {
@@ -87,6 +88,35 @@ namespace SMS
             MainWindow.Show();
 
             base.OnStartup(e);
+
+            LiveCharts.Configure(config =>
+            config
+                // you can override the theme 
+                // .AddDarkTheme()  
+
+                // In case you need a non-Latin based font, you must register a typeface for SkiaSharp
+                //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('汉')) // <- Chinese 
+                //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('あ')) // <- Japanese 
+                //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('헬')) // <- Korean 
+                //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('Ж'))  // <- Russian 
+
+                //.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('أ'))  // <- Arabic 
+                //.UseRightToLeftSettings() // Enables right to left tooltips 
+
+                // finally register your own mappers
+                // you can learn more about mappers at:
+                // https://livecharts.dev/docs/wpf/2.0.0-rc1/Overview.Mappers
+                .HasMap<City>((city, point) =>
+                {
+                    // here we use the index as X, and the population as Y 
+                    point.Coordinate = new(point.Index, city.Population);
+                })
+            // .HasMap<Foo>( .... ) 
+            // .HasMap<Bar>( .... ) 
+            );
         }
+
+        public record City(string Name, double Population);
+    
     }
 }
--- SMS/ViewModels/HomeViewModel.cs
+++ SMS/ViewModels/HomeViewModel.cs
@@ -1,4 +1,10 @@
-﻿using SMS.Model.Db.Entities;
+﻿using LiveChartsCore;
+using LiveChartsCore.Measure;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+using SMS.Model.Db.Entities;
 using SMS.Model.Repositories;
 using SMS.Model.Responses;
 using SMS.Util;
@@ -24,6 +30,84 @@ namespace SMS.ViewModels
         //    set { _mainVm = value; }
         //}
 
+        static SKColor skRed = new SKColor(237, 128, 217);
+        static SKColor skBlue = new SKColor(64, 58, 107);
+
+
+
+        public ISeries[] Line1Series { get; set; }
+            = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Name = "Students",
+                    Values = new double[] { 2, 2, 4, 5, 5, 7, 7 },
+                    Fill = null
+                },
+                new LineSeries<double>
+                {
+                    Name = "Staff",
+                    Values = new double[] { 1, 2, 2, 3, 3, 4, 4 },
+                    Fill = null
+                },
+                new LineSeries<double>
+                {
+                    Name = "Workers",
+                    Values = new double[] { 3, 4, 4, 6, 6, 8, 10 },
+                    Fill = null
+                }
+            };
+
+        public IEnumerable<ISeries> Guage1 { get; set; } =
+        GaugeGenerator.BuildSolidGauge(
+            new GaugeItem(
+                40,          // the gauge value
+                series =>    // the series style
+                {
+                    series.Name = "Students";
+                    series.MaxRadialColumnWidth = 20;
+                    series.DataLabelsSize = 35;
+                    series.OuterRadiusOffset = 10;
+                    series.InnerRadius = 10;
+                    series.Fill = new SolidColorPaint(skRed);
+                    series.DataLabelsPaint = new SolidColorPaint(skBlue);
+                    series.DataLabelsPosition = PolarLabelsPosition.ChartCenter;
+                }));
+        public int Guage1MaxValue => 100;
+
+        public IEnumerable<ISeries> Guage2 { get; set; } =
+        GaugeGenerator.BuildSolidGauge(
+            new GaugeItem(
+                30,          // the gauge value
+                series =>    // the series style
+                {
+                    series.Name = "Staff";
+                    series.MaxRadialColumnWidth = 20;
+                    series.DataLabelsSize = 35;
+                    series.OuterRadiusOffset = 10;
+                    series.InnerRadius = 10;
+                    series.Fill = new SolidColorPaint(skRed);
+                    series.DataLabelsPaint = new SolidColorPaint(skBlue);
+                    series.DataLabelsPosition = PolarLabelsPosition.ChartCenter;
+                }));
+        public int Guage2MaxValue => 100;
+
+        public IEnumerable<ISeries> Guage3 { get; set; } =
+        GaugeGenerator.BuildSolidGauge(
+            new GaugeItem(
+                60,          // the gauge value
+                series =>    // the series style
+                {
+                    series.Name = "Workers";
+                    series.MaxRadialColumnWidth = 20;
+                    series.DataLabelsSize = 35;
+                    series.OuterRadiusOffset = 10;
+                    series.InnerRadius = 10;
+                    series.Fill = new SolidColorPaint(skRed);
+                    series.DataLabelsPaint = new SolidColorPaint(skBlue);
+                    series.DataLabelsPosition = PolarLabelsPosition.ChartCenter;
+                }));
+        public int Guage3MaxValue => 100;
 
         public HomeViewModel(NavigationService<DiscoveryViewModel> discoveryNavigationService, HomeRepository repository)
         {
